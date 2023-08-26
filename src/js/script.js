bdc89a469e1ac657124b46a91886f6d9276f46db
inami-code <inami.design@gmail.com>
2023-08-26 14:12:10 +0900

    0826

--- src/js/script.js
+++ src/js/script.js
@@ -95,21 +95,31 @@ topBtn.click(function () {
   // スムーススクロール（headerに被らない）
   // ------------------------------------
 
-  $(function () {
-    const height = $(".js-header").height();
-    $("main").css("margin-top", height);
-  });
-  // ページ内スクロール
-  $(function () {
-    $('a[href^="#"]').click(function () {
-      const speed = 600;
-      let href = $(this).attr("href");
-      let target = $(href == "#" || href == "" ? "html" : href);
-      let position = target.offset().top;
-      $("body,html").animate({ scrollTop: position }, speed, "swing");
-      return false;
-    });
+  $(document).on("click", 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $("header").innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $("html,body").animate({ scrollTop: targetY }, time, "swing");
+    return false;
   });
 
+
+  $('a[href^="#"]').click(function(){
+    // 移動先を0px調整する。0を30にすると30px下にずらすことができる。
+    var adjust = 300;
+    // スクロールの速度（ミリ秒）
+    var speed = 400;
+    // アンカーの値取得 リンク先（href）を取得して、hrefという変数に代入
+    var href= $(this).attr("href");
+    // 移動先を取得 リンク先(href）のidがある要素を探して、targetに代入
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    // 移動先を調整 idの要素の位置をoffset()で取得して、positionに代入
+    var position = target.offset().top + adjust;
+    // スムーススクロール linear（等速） or swing（変速）
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
 
 });
